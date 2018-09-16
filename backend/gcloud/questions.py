7986f526cdf8136af1a7eb85e12c22cdfa929983
hanksterhan <henryhan62@gmail.com>
2018-09-15 22:33:06 -0400

    moved questions

--- backend/gcloud/questions.py
+++ backend/gcloud/questions.py
@@ -1,3 +1,4 @@
+import random
 class QuestionGeneration(object):
     def __init__(self):
         pass
--- backend/question_generation.py
+++ backend/question_generation.py
@@ -9,30 +9,30 @@ from questions import QuestionGeneration
     argv[3] : user text
 """
 
-# def extract_keywords(new_text):
-#     """ takes new user text and returns a list of keywords
-#     """
-#     headers = {'Ocp-Apim-Subscription-Key': '59bf3be460ad434585a4b4143c470a92'}
-#     document = {'documents': [{'id': '1', 'text': new_text}]}
-#     azure_url = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/KeyPhrases"
-#
-#     response = requests.post(azure_url, headers=headers, json=document)
-#     response = response.json()
-#     return [keyword.encode('utf-8') for keyword in response['documents'][0]['keyPhrases']]
-#
-# def extract_date_location(keyword_entry, date_location):
-#     """ Given a keywords entry in the keyword_dict, return either the most recent date or location
-#     """
-#     data_len = len(keyword_entry[date_location])
-#     # if only one date, return that date
-#     if data_len == 1:
-#         return keyword_entry[date_location][0]
-#     else:
-#         # 70% of the time, grab the most recent date
-#         if randint(0,9) < 7:
-#             return keyword_entry[date_location][-1]
-#         else: # grab a random date in the list
-#             return keyword_entry[date_location][randint(0,data_len)]
+def extract_keywords(new_text):
+    """ takes new user text and returns a list of keywords
+    """
+    headers = {'Ocp-Apim-Subscription-Key': '59bf3be460ad434585a4b4143c470a92'}
+    document = {'documents': [{'id': '1', 'text': new_text}]}
+    azure_url = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/KeyPhrases"
+
+    response = requests.post(azure_url, headers=headers, json=document)
+    response = response.json()
+    return [keyword.encode('utf-8') for keyword in response['documents'][0]['keyPhrases']]
+
+def extract_date_location(keyword_entry, date_location):
+    """ Given a keywords entry in the keyword_dict, return either the most recent date or location
+    """
+    data_len = len(keyword_entry[date_location])
+    # if only one date, return that date
+    if data_len == 1:
+        return keyword_entry[date_location][0]
+    else:
+        # 70% of the time, grab the most recent date
+        if randint(0,9) < 7:
+            return keyword_entry[date_location][-1]
+        else: # grab a random date in the list
+            return keyword_entry[date_location][randint(0,data_len)]
 
 
 def main():
@@ -41,24 +41,25 @@ def main():
     corpus = ast.literal_eval(sys.argv[2])
     new_text = sys.argv[3]
 
-    # # instantiate question generator:
-    # qqgen = QuestionGeneration()
-    # questions = []
-    # # extract keywords
+    # instantiate question generator:
+    qqgen = QuestionGeneration()
+    questions = []
+    # extract keywords
     # keywords = extract_keywords(new_text)
-    # for keyword in keywords:
-    #     if keyword in keyword_dict: # the keyword exists in the dictionary already
-    #         date = extract_date_location(keyword_dict[keyword], 'date')
-    #
-    #         location = extract_date_location(keyword_dict[keyword], 'location')
-    #         questions.append(qqgen.askLocationQ(location))
-    #
-    #         if len(keyword_dict['activity']) != 0:
-    #             questions.append(qqgen.askActivityQ(keyword_dict['activity']))
-    #
+    keywords = 'swim'
+    for keyword in keywords:
+        if keyword in keyword_dict: # the keyword exists in the dictionary already
+            date = extract_date_location(keyword_dict[keyword], 'date')
 
-    # print questions
-    print keyword_dict
+            location = extract_date_location(keyword_dict[keyword], 'location')
+            questions.append(qqgen.askLocationQ(location))
+
+            if len(keyword_dict['activity']) != 0:
+                questions.append(qqgen.askActivityQ(keyword_dict['activity']))
+
+
+    print questions
+    print keyword_dict[keywords]
     sys.stdout.flush()
 
 if __name__ == '__main__':
