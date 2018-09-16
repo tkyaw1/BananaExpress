2225b6a6176c1809d8b4c638516d6468e82cee87
hanksterhan <henryhan62@gmail.com>
2018-09-16 06:20:15 -0400

    debugged questions

--- backend/questions_backend.py
+++ backend/questions_backend.py
@@ -11,9 +11,21 @@ class QuestionGeneration(object):
         "What were you grateful for today?",
         "Did you notice anything different today?",
         "What did you accomplish today?",
+        "Did you find a chance to get outside today?",
+        "Were you able to exercise today?",
+        "What are a few words that capture today?",
+        "How do you feel today?",
+        "Did anyone inspire you today?",
+        "Did you learn anything new today?",
+        "Are you feeling stressed?",
+        "Any good music recently?",
+        "Did anything inspire you today?",
         "What is something you want to do tomorrow?",
         "What is something you want to do this weekend?",
-        "What is something you want to do soon?"])
+        "What is something you want to do soon?",
+        "Were you reminded of home today?",
+        "What is something you want to work on tomorrow?",
+        "Did you get enough sleep recently?"])
 
     def askLocationQ(self, location):
         return random.choice(["What did you do in %s?" %location,
@@ -21,19 +33,28 @@ class QuestionGeneration(object):
         "What did you eat in %s while you were there?" %location,
         "Who did you meet in %s?" %location,
         "First time in %s?" %location,
-        "Have you ever been to %s before?" %location])
+        "Have you ever been to %s before?" %location,
+        "Did going to %s inspire you to go somewhere?" %location])
 
     def askActivityQ(self, activity):
         return random.choice(["How did %s go?" %activity,
         "Can you tell me more about your %s experience?" %activity,
-        "Was %s fun?" %activity])
+        "Was %s fun?" %activity,
+        "Do you want to go %s again soon?" %activity,
+        "Are there other good places to go %s nearby?" %activity])
 
     def askFoodQ(self, food):
         return random.choice(["How was your meal?",
-        "Have a good meal?", "Would you get the same meal again?"])
+        "Have a good meal?",
+        "Would you get the same meal again?",
+        "How does this compare to similar restaurants?",
+        "How was the dining service?",
+        "How was the dining atmosphere?",
+        "Was there something else on the menu you wanted to try?"])
 
-    def askDateVerbQ(self, date):
-        return random.choice([""])
+    def askDateVerbQ(self, date, verb):
+        return random.choice(["You went %s on %s as well, how does it compare?" %(verb, date),
+        "Do you wish you went %s more often?" %verb])
 
     def askPeopleQ(self):
         return random.choice(["With friends?",
@@ -42,4 +63,5 @@ class QuestionGeneration(object):
     def askCompActivityQ(self, verb_ing, date, location):
         """ Given that the verb was an ing verb:
         """
-        return random.choice(["How was %s this time compared to %s on %s at %s?" % (verb_ing, verb_ing, date, location)]) #TODO: need to add more to this
+        return random.choice(["How was %s this time compared to %s on %s at %s?" %(verb_ing, verb_ing, date, location),
+        "You went %s on %s as well, how does it compare?" %(verb_ing, date), "How does %s at %s compare?" %(verb_ing, location)])
