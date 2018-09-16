bb50379713146cc891253b6872c72e475b4237c7
hanksterhan <henryhan62@gmail.com>
2018-09-16 10:12:49 -0400

    Machine Learning model for text generation from images for captioning

---
+++ backend/ML_pipe.py
@@ -0,0 +1,116 @@
+from os import listdir
+from pickle import dump
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+import string
+
+import pandas as pd
+
+def create_dictionary():
+    df = pd.read_csv('CSVforHank/coco_train_all.csv')
+    df = df.loc[:,['image_id','questions']]
+    dictionary = {}
+    for row in df.iterrows():
+        dictionary[row[1]['image_id']] = row[1]['questions'].split("---")
+    return dictionary
+
+# extract features from each photo in the directory
+def extract_features(directory):
+	# load the model
+	model = VGG16()
+	# re-structure the model
+	model.layers.pop()
+	model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+	# summarize
+	print(model.summary())
+	# extract features from each photo
+	features = dict()
+	for name in listdir(directory):
+		# load an image from file
+		filename = directory + '/' + name
+		image = load_img(filename, target_size=(224, 224))
+		# convert the image pixels to a numpy array
+		image = img_to_array(image)
+		# reshape data for the model
+		image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+		# prepare the image for the VGG model
+		image = preprocess_input(image)
+		# get features
+		feature = model.predict(image, verbose=0)
+		# get image id
+		image_id = name.split('.')[0]
+		# store feature
+		features[image_id] = feature
+		print('>%s' % name)
+	return features
+
+def clean_descriptions(descriptions):
+	# prepare translation table for removing punctuation
+	table = str.maketrans('', '', string.punctuation)
+    # table = [w.translate(None, string.punctuation) for w in descriptions]
+
+	for key, desc_list in descriptions.items():
+		for i in range(len(desc_list)):
+			desc = desc_list[i]
+			# tokenize
+			desc = desc.split()
+			# convert to lower case
+			desc = [word.lower() for word in desc]
+			# remove punctuation from each token
+			desc = [w.translate(table) for w in desc]
+			# remove hanging 's' and 'a'
+			desc = [word for word in desc if len(word)>1]
+			# remove tokens with numbers in them
+			desc = [word for word in desc if word.isalpha()]
+			# store as string
+			desc_list[i] =  ' '.join(desc)
+
+# convert the loaded descriptions into a vocabulary of words
+def to_vocabulary(descriptions):
+	# build a list of all description strings
+	all_desc = set()
+	for key in descriptions.keys():
+		[all_desc.update(d.split()) for d in descriptions[key]]
+	return all_desc
+
+# save descriptions to file, one per line
+def save_descriptions(descriptions, filename):
+	lines = list()
+	for key, desc_list in descriptions.items():
+		for desc in desc_list:
+			lines.append(str(key) + ' ' + desc)
+	data = '\n'.join(lines)
+	file = open(filename, 'w')
+	file.write(data)
+	file.close()
+
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+    
+descriptions = create_dictionary()
+clean_descriptions(descriptions)
+# summarize vocabulary
+vocabulary = to_vocabulary(descriptions)
+print('Vocabulary Size: %d' % len(vocabulary))
+# save to file
+save_descriptions(descriptions, 'descriptions.txt')
+
+# extract features from all images
+features = extract_features("/mnt/c/Users/hhan/Documents/flicker_train_img/")
+print('Extracted Features: %d' % len(features))
+# save to file
+dump(features, open('features.pkl', 'wb'))
+
+# parse descriptions #QUESTIONS:
+descriptions = load_questions(doc)
+print('Loaded: %d ' % len(descriptions))
